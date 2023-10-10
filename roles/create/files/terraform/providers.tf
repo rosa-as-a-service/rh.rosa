3d214ff9fb0d44ee82add9a43695f6faf56d2494
Alex Hussey <null>
2023-10-10 06:31:06 +0000

    Bump RHCS provider version

--- roles/create/files/terraform/providers.tf
+++ roles/create/files/terraform/providers.tf
@@ -5,7 +5,7 @@ terraform {
       version = ">= 4.0.0"
     }
     rhcs = {
-      version = "= 1.4.0-prerelease.2"
+      version = "= 1.4.0-prerelease.3"
       source  = "terraform-redhat/rhcs"
     }
   }
