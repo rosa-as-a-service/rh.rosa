0292884de3e7302ec73d334d3a3a008494573853
Alex Hussey <alex@blueguardian.co>
2023-10-12 11:59:42 +1100

    Restructure variables

--- roles/create/templates/data.tf
+++ roles/create/templates/data.tf
@@ -12,14 +12,14 @@ data "rhcs_versions" "all" {}
 data "aws_vpc" "tenent_vpc" {
   filter {
     name   = "tag:Name"
-    values = ["{{ rosa_vpc }}"]
+    values = ["{{ rosa_vpc_name }}"]
   }
 }
 
 data "aws_subnets" "tenent_subnet_ids" {
   filter {
     name   = "tag:Name"
-    values = ["{{ rosa_subnet_1 | default(rosa_vpc + '-2a') }}", "{{ rosa_subnet_2 | default(rosa_vpc + '-2b') }}"]
+    values = ["{{ rosa_subnets[0].name | default(rosa_vpc_name + '-2a') }}", "{{ rosa_subnets[1].name | default(rosa_vpc_name + '-2b') }}"]
   }
 }
 
