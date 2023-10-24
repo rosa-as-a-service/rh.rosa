38692bd24858a91fb40432d6f8801f313b34fedc
Alex Hussey <ahussey@redhat.com>
2023-10-25 09:30:13 +1100

    Cleanup PrivateLink Terraform

--- roles/bootstrap_spoke/files/terraform/privatelink.tf
+++ roles/bootstrap_spoke/files/terraform/privatelink.tf
@@ -12,47 +12,25 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
 ## Modify spoke#-infraid-master-sg securitygroup to allow the Hub subnet to consume 6443/tcp
 resource "aws_security_group_rule" "allow_hub" {
-  name        = "allow_hub"
-  description = "Allow Kubernetes API inbound traffic"
-  vpc_id      = "${aws_vpc.spoke_vpc.id}"
-
-    description      = "Kubernetes API from Spoke"
-    from_port        = any
-    to_port          = 6443
-    protocol         = "tcp"
-    type             = "Custom TCP"
-    cidr_blocks      = ["${aws_vpc.hub_vpc.cidr_block}"]
-
-  tags = {
-    Name = "allow_hub"
-  }
+  description = "Allow Kubernetes API inbound traffic from Hub"
+  security_group_id = "${aws_security_group.spoke_master_security_group.id}"
+  from_port        = any
+  to_port          = 6443
+  protocol         = "tcp"
+  type             = "Custom TCP"
+  cidr_blocks      = ["${aws_vpc.hub_vpc.cidr_block}"]
 }
 
 ## Modify hub-infraid-master-sg securitygroup to allow the Spoke subnet to consume 6443/tcp
 resource "aws_security_group_rule" "allow_$${ var.rosa_cluster_name }" {
-  name        = "allow_${ var.rosa_cluster_name }"
-  description = "Allow Kubernetes API inbound traffic"
-  vpc_id      = "${aws_vpc.hub_vpc.id}""
+  description = "Allow Kubernetes API inbound traffic from ${ var.rosa_cluster_name }"
+  security_group_id = "${aws_security_group.hub_master_security_group.id}"
+  from_port        = any
+  to_port          = 6443
+  protocol         = "tcp"
+  type             = "Custom TCP"
+  cidr_blocks      = ["${aws_vpc.spoke_vpc.cidr_block}"]
 
-  ingress {
-    description      = "Kubernetes API from ${ var.rosa_cluster_name }"
-    from_port        = 6443
-    to_port          = 6443
-    protocol         = "tcp"
-    cidr_blocks      = ["${aws_vpc.spoke_vpc.cidr_block}"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_${ var.rosa_cluster_name }"
-  }
 }
 
 ## Need to associate security groups with the `master` nodes
@@ -67,26 +45,24 @@ resource "aws_vpc_endpoint_service" "$${ var.rosa_cluster_name }" {
 resource "aws_route53_record" "spoke_base_domain_verification" {
   zone_id = "${var.hosted_zone_id}"
   name    = "${var.rosa_cluster_name }.private_dns_name_configuration.name"
-  value   = "${var.rosa_cluster_name }.private_dns_name_configuration.value"
+  records   = "${var.rosa_cluster_name }.private_dns_name_configuration.value"
   type    = "TXT"
   ttl     = 1800
 }
 
 resource "time_sleep" "wait_for_base_domain_dns_propogration" {
-  depends_on      = [${aws_route53_record.spoke_base_domain_verification}]
+  depends_on      = ["${aws_route53_record.spoke_base_domain_verification}"]
   create_duration = "180s"
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_endpoint
 ## Create a VPC Endpoint in the Spoke to consume 6443/tcp from the Hub via PrivateLink
 resource "aws_vpc_endpoint" "hub" {
-  vpc_id            = ${data.aws_vpc.spoke_vpc.id}
+  vpc_id            = "${data.aws_vpc.spoke_vpc.id}"
   service_name      = "${data.aws_vpc_endpoint_service.hub_endpoint_service.name}"
   vpc_endpoint_type = "Interface"
 
-  security_group_ids = [
-    ${aws_security_group.allow_hub.id},
-  ]
+  security_group_ids = ["${aws_security_group.allow_hub.id}"]
 
   private_dns_enabled = true
 }
@@ -97,9 +73,7 @@ resource "aws_vpc_endpoint" "$${ var.rosa_cluster_name }" {
   service_name      = "${data.aws_vpc_endpoint_service.spoke_endpoint_service.name}"
   vpc_endpoint_type = "Interface"
 
-  security_group_ids = [
-    ${aws_security_group.allow_${ var.rosa_cluster_name }.id},
-  ]
+  security_group_ids = ["${aws_security_group.allow_${ var.rosa_cluster_name }.id}"]
 
   private_dns_enabled = true
 }
