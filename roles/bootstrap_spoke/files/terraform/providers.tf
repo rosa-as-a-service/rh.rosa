9eb38fe3d74689e3255e41a5328a66ddd6f5a653
Alex Hussey <ahussey@redhat.com>
2023-10-25 14:07:28 +1100

    Debug AWS provider

--- roles/bootstrap_spoke/files/terraform/providers.tf
+++ roles/bootstrap_spoke/files/terraform/providers.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0.0"
+      source = "hashicorp/aws"
+      version = "5.22.0"
     }
   }
 }
 
 provider "aws" {
   region                 = "ap-southeast-2"
-  access_key             = var.AWS_ACCESS_KEY_ID
-  secret_key             = var.AWS_SECRET_ACCESS_KEY
+  access_key             = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key             = "${var.AWS_SECRET_ACCESS_KEY}"
   skip_region_validation = true
   ignore_tags {
     key_prefixes = ["kubernetes.io/"]
