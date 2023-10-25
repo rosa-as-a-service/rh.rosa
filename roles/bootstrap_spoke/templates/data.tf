70234844e9002987adc36cc02775a8608b55e63f
Alex Hussey <ahussey@redhat.com>
2023-10-25 15:38:09 +1100

    Move data.tf back to templates

--- roles/bootstrap_spoke/templates/data.tf
+++ roles/bootstrap_spoke/templates/data.tf
@@ -3,14 +3,14 @@ data "aws_caller_identity" "current" {}
 data "aws_vpc" "spoke_vpc" {
   filter {
     name   = "tag:cluster-name"
-    values = [ var.rosa_cluster_name ]
+    values = [ "{{ rosa_cluster_name }}" ]
   }
 }
 
 data "aws_vpc_endpoint_service" "spoke_endpoint_service" {
   filter {
     name   = "tag:hive.openshift.io/private-link-access-for"
-    values = [ var.rosa_cluster_infra_id ]
+    values = [ "{{ _rosa_cluster_infra_id }}" ]
   }
 }
 
@@ -24,26 +24,26 @@ data "aws_vpc" "hub_vpc" {
 data "aws_vpc_endpoint_service" "hub_endpoint_service" {
   filter {
     name   = "tag:hive.openshift.io/private-link-access-for"
-    values = [ var.rosa_hub_cluster_infra_id ]
+    values = [ "{{ _rosa_hub_cluster_infra_id }}" ]
   }
 }
 
 data "aws_security_group" "spoke_master_security_group" {
   tags = {
-    Name = "${var.rosa_cluster_infra_id}-master-sg"
+    Name = "{{ _rosa_cluster_infra_id }}-master-sg"
   }
 }
 
 data "aws_security_group" "hub_master_security_group" {
   tags = {
-    Name = "${var.rosa_hub_cluster_infra_id}-master-sg"
+    Name = "{{ _rosa_hub_cluster_infra_id }}-master-sg"
   }
 }
 
 data "aws_lb" "spoke_lb" {
-  name = "${var.rosa_cluster_infra_id}-int"
+  name = "{{ _rosa_cluster_infra_id }}-int"
 }
 
 data "aws_route53_zone" "spoke_hosted_zone" {
-  name = "${var.rosa_base_domain}."
+  name = "{{ _rosa_base_domain }}."
 }
