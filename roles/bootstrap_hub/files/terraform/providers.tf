df2c4b17c482ab369e024a55d3dc471efb7fc261
Alex Hussey <ahussey@redhat.com>
2023-10-31 16:17:35 +1100

    Add S3 Terraform backend

--- roles/bootstrap_hub/files/terraform/providers.tf
+++ roles/bootstrap_hub/files/terraform/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.22.0"
     }
   }
+  backend "s3" {
+    bucket = "${var.rosa_cluster_name}-terraform"
+    key    = "privatelink/terraform.tfstate"
+    region = "ap-southeast-2"
+    access_key             = "${var.AWS_ACCESS_KEY_ID}"
+    secret_key             = "${var.AWS_SECRET_ACCESS_KEY}"
+  }
 }
 
 provider "aws" {
@@ -16,4 +23,3 @@ provider "aws" {
     key_prefixes = ["kubernetes.io/"]
   }
 }
-
--- roles/delete_hub/files/rosa/terraform/providers.tf
+++ roles/delete_hub/files/rosa/terraform/providers.tf
@@ -9,6 +9,13 @@ terraform {
       version = "1.4.0"
     }
   }
+  backend "s3" {
+    bucket = "${var.rosa_cluster_name}-terraform"
+    key    = "rosa/terraform.tfstate"
+    region = "ap-southeast-2"
+    access_key             = "${var.AWS_ACCESS_KEY_ID}"
+    secret_key             = "${var.AWS_SECRET_ACCESS_KEY}"
+  }
 }
 
 provider "rhcs" {
